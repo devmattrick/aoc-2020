490fd10153fefca79dbf2ba79420ee3e32b7985b
Matthew McCune <matthew@matthew.cx>
2020-12-07 04:04:41 -0800

    Complete day 7

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[macro_use] extern crate lazy_static;
 #[allow(unused_imports)]
 #[macro_use] extern crate aoc_runner_derive;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 mod days;
 mod macros;
 
